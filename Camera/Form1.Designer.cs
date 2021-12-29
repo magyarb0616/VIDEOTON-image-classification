b15bc9ed00c4c084505814b90dfdb773614c9cc3
magyarb0616 <magyarb0616@gmail.com>
2021-12-29 17:25:47 +0100

    Add project files.

---
+++ Camera/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace Camera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.Satart_button = new System.Windows.Forms.Button();
+			this.Snapshot_button = new System.Windows.Forms.Button();
+			this.CameraList_comboBox = new System.Windows.Forms.ComboBox();
+			this.Capture_pictureBox = new System.Windows.Forms.PictureBox();
+			this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+			this.Result_Label = new System.Windows.Forms.Label();
+			this.gbValidationPanel = new System.Windows.Forms.GroupBox();
+			this.btnCorrect = new System.Windows.Forms.Button();
+			this.btnPPN = new System.Windows.Forms.Button();
+			this.btnPNP = new System.Windows.Forms.Button();
+			this.btnPNN = new System.Windows.Forms.Button();
+			this.btnPPP = new System.Windows.Forms.Button();
+			this.btnNPP = new System.Windows.Forms.Button();
+			this.btnNPN = new System.Windows.Forms.Button();
+			this.btnNNP = new System.Windows.Forms.Button();
+			this.btnNNN = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.pbVideoton = new System.Windows.Forms.PictureBox();
+			this.pbInstagrid = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.Capture_pictureBox)).BeginInit();
+			this.gbValidationPanel.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbVideoton)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbInstagrid)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// Satart_button
+			// 
+			this.Satart_button.Location = new System.Drawing.Point(512, 50);
+			this.Satart_button.Margin = new System.Windows.Forms.Padding(0);
+			this.Satart_button.Name = "Satart_button";
+			this.Satart_button.Size = new System.Drawing.Size(180, 45);
+			this.Satart_button.TabIndex = 1;
+			this.Satart_button.Text = "Start";
+			this.Satart_button.UseVisualStyleBackColor = true;
+			this.Satart_button.Click += new System.EventHandler(this.Satart_button_Click);
+			// 
+			// Snapshot_button
+			// 
+			this.Snapshot_button.BackColor = System.Drawing.SystemColors.Control;
+			this.Snapshot_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.Snapshot_button.Location = new System.Drawing.Point(1604, 192);
+			this.Snapshot_button.Margin = new System.Windows.Forms.Padding(6);
+			this.Snapshot_button.Name = "Snapshot_button";
+			this.Snapshot_button.Size = new System.Drawing.Size(627, 195);
+			this.Snapshot_button.TabIndex = 2;
+			this.Snapshot_button.Text = "Snapshot";
+			this.Snapshot_button.UseVisualStyleBackColor = false;
+			this.Snapshot_button.Click += new System.EventHandler(this.Snapshot_button_Click);
+			// 
+			// CameraList_comboBox
+			// 
+			this.CameraList_comboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.CameraList_comboBox.FormattingEnabled = true;
+			this.CameraList_comboBox.Location = new System.Drawing.Point(37, 50);
+			this.CameraList_comboBox.Margin = new System.Windows.Forms.Padding(6);
+			this.CameraList_comboBox.Name = "CameraList_comboBox";
+			this.CameraList_comboBox.Size = new System.Drawing.Size(469, 45);
+			this.CameraList_comboBox.TabIndex = 3;
+			// 
+			// Capture_pictureBox
+			// 
+			this.Capture_pictureBox.Location = new System.Drawing.Point(812, 192);
+			this.Capture_pictureBox.Margin = new System.Windows.Forms.Padding(6);
+			this.Capture_pictureBox.Name = "Capture_pictureBox";
+			this.Capture_pictureBox.Size = new System.Drawing.Size(728, 546);
+			this.Capture_pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.Capture_pictureBox.TabIndex = 4;
+			this.Capture_pictureBox.TabStop = false;
+			// 
+			// videoSourcePlayer1
+			// 
+			this.videoSourcePlayer1.Location = new System.Drawing.Point(37, 192);
+			this.videoSourcePlayer1.Margin = new System.Windows.Forms.Padding(6);
+			this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+			this.videoSourcePlayer1.Size = new System.Drawing.Size(728, 546);
+			this.videoSourcePlayer1.TabIndex = 5;
+			this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+			this.videoSourcePlayer1.VideoSource = null;
+			this.videoSourcePlayer1.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer1_NewFrame);
+			// 
+			// Result_Label
+			// 
+			this.Result_Label.AutoSize = true;
+			this.Result_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.Result_Label.Location = new System.Drawing.Point(1007, 123);
+			this.Result_Label.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+			this.Result_Label.Name = "Result_Label";
+			this.Result_Label.Size = new System.Drawing.Size(310, 63);
+			this.Result_Label.TabIndex = 6;
+			this.Result_Label.Text = "Prediction: ";
+			// 
+			// gbValidationPanel
+			// 
+			this.gbValidationPanel.Controls.Add(this.btnCorrect);
+			this.gbValidationPanel.Controls.Add(this.btnPPN);
+			this.gbValidationPanel.Controls.Add(this.btnPNP);
+			this.gbValidationPanel.Controls.Add(this.btnPNN);
+			this.gbValidationPanel.Controls.Add(this.btnPPP);
+			this.gbValidationPanel.Controls.Add(this.btnNPP);
+			this.gbValidationPanel.Controls.Add(this.btnNPN);
+			this.gbValidationPanel.Controls.Add(this.btnNNP);
+			this.gbValidationPanel.Controls.Add(this.btnNNN);
+			this.gbValidationPanel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.gbValidationPanel.Location = new System.Drawing.Point(1604, 439);
+			this.gbValidationPanel.Name = "gbValidationPanel";
+			this.gbValidationPanel.Size = new System.Drawing.Size(643, 299);
+			this.gbValidationPanel.TabIndex = 7;
+			this.gbValidationPanel.TabStop = false;
+			this.gbValidationPanel.Text = "Prediction Validation";
+			// 
+			// btnCorrect
+			// 
+			this.btnCorrect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.btnCorrect.ForeColor = System.Drawing.Color.LimeGreen;
+			this.btnCorrect.Location = new System.Drawing.Point(404, 41);
+			this.btnCorrect.Name = "btnCorrect";
+			this.btnCorrect.Size = new System.Drawing.Size(217, 242);
+			this.btnCorrect.TabIndex = 8;
+			this.btnCorrect.Text = "CORRECT!";
+			this.btnCorrect.UseVisualStyleBackColor = true;
+			this.btnCorrect.Click += new System.EventHandler(this.btnCorrect_Click);
+			// 
+			// btnPPN
+			// 
+			this.btnPPN.ForeColor = System.Drawing.Color.Red;
+			this.btnPPN.Location = new System.Drawing.Point(215, 227);
+			this.btnPPN.Name = "btnPPN";
+			this.btnPPN.Size = new System.Drawing.Size(183, 56);
+			this.btnPPN.TabIndex = 7;
+			this.btnPPN.Text = "PPN";
+			this.btnPPN.UseVisualStyleBackColor = true;
+			this.btnPPN.Click += new System.EventHandler(this.btnPPN_Click);
+			// 
+			// btnPNP
+			// 
+			this.btnPNP.ForeColor = System.Drawing.Color.Red;
+			this.btnPNP.Location = new System.Drawing.Point(215, 165);
+			this.btnPNP.Name = "btnPNP";
+			this.btnPNP.Size = new System.Drawing.Size(183, 56);
+			this.btnPNP.TabIndex = 6;
+			this.btnPNP.Text = "PNP";
+			this.btnPNP.UseVisualStyleBackColor = true;
+			this.btnPNP.Click += new System.EventHandler(this.btnPNP_Click);
+			// 
+			// btnPNN
+			// 
+			this.btnPNN.ForeColor = System.Drawing.Color.Red;
+			this.btnPNN.Location = new System.Drawing.Point(215, 103);
+			this.btnPNN.Name = "btnPNN";
+			this.btnPNN.Size = new System.Drawing.Size(183, 56);
+			this.btnPNN.TabIndex = 5;
+			this.btnPNN.Text = "PNN";
+			this.btnPNN.UseVisualStyleBackColor = true;
+			this.btnPNN.Click += new System.EventHandler(this.btnPNN_Click);
+			// 
+			// btnPPP
+			// 
+			this.btnPPP.ForeColor = System.Drawing.Color.Red;
+			this.btnPPP.Location = new System.Drawing.Point(215, 41);
+			this.btnPPP.Name = "btnPPP";
+			this.btnPPP.Size = new System.Drawing.Size(183, 56);
+			this.btnPPP.TabIndex = 4;
+			this.btnPPP.Text = "PPP";
+			this.btnPPP.UseVisualStyleBackColor = true;
+			this.btnPPP.Click += new System.EventHandler(this.btnPPP_Click);
+			// 
+			// btnNPP
+			// 
+			this.btnNPP.ForeColor = System.Drawing.Color.Red;
+			this.btnNPP.Location = new System.Drawing.Point(26, 227);
+			this.btnNPP.Name = "btnNPP";
+			this.btnNPP.Size = new System.Drawing.Size(183, 56);
+			this.btnNPP.TabIndex = 3;
+			this.btnNPP.Text = "NPP";
+			this.btnNPP.UseVisualStyleBackColor = true;
+			this.btnNPP.Click += new System.EventHandler(this.btnNPP_Click);
+			// 
+			// btnNPN
+			// 
+			this.btnNPN.ForeColor = System.Drawing.Color.Red;
+			this.btnNPN.Location = new System.Drawing.Point(26, 165);
+			this.btnNPN.Name = "btnNPN";
+			this.btnNPN.Size = new System.Drawing.Size(183, 56);
+			this.btnNPN.TabIndex = 2;
+			this.btnNPN.Text = "NPN";
+			this.btnNPN.UseVisualStyleBackColor = true;
+			this.btnNPN.Click += new System.EventHandler(this.btnNPN_Click);
+			// 
+			// btnNNP
+			// 
+			this.btnNNP.ForeColor = System.Drawing.Color.Red;
+			this.btnNNP.Location = new System.Drawing.Point(26, 103);
+			this.btnNNP.Name = "btnNNP";
+			this.btnNNP.Size = new System.Drawing.Size(183, 56);
+			this.btnNNP.TabIndex = 1;
+			this.btnNNP.Text = "NNP";
+			this.btnNNP.UseVisualStyleBackColor = true;
+			this.btnNNP.Click += new System.EventHandler(this.btnNNP_Click);
+			// 
+			// btnNNN
+			// 
+			this.btnNNN.ForeColor = System.Drawing.Color.Red;
+			this.btnNNN.Location = new System.Drawing.Point(26, 41);
+			this.btnNNN.Name = "btnNNN";
+			this.btnNNN.Size = new System.Drawing.Size(183, 56);
+			this.btnNNN.TabIndex = 0;
+			this.btnNNN.Text = "NNN";
+			this.btnNNN.UseVisualStyleBackColor = true;
+			this.btnNNN.Click += new System.EventHandler(this.btnNNN_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.label1.Location = new System.Drawing.Point(209, 123);
+			this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(356, 63);
+			this.label1.TabIndex = 8;
+			this.label1.Text = "Live preview:";
+			// 
+			// chart
+			// 
+			chartArea1.Name = "ChartArea1";
+			this.chart.ChartAreas.Add(chartArea1);
+			legend1.Name = "Legend1";
+			this.chart.Legends.Add(legend1);
+			this.chart.Location = new System.Drawing.Point(812, 771);
+			this.chart.Name = "chart";
+			this.chart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Pastel;
+			series1.ChartArea = "ChartArea1";
+			series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+			series1.Font = new System.Drawing.Font("Calibri", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			series1.Legend = "Legend1";
+			series1.Name = "s1";
+			this.chart.Series.Add(series1);
+			this.chart.Size = new System.Drawing.Size(821, 546);
+			this.chart.TabIndex = 11;
+			this.chart.Text = "chart";
+			// 
+			// pbVideoton
+			// 
+			this.pbVideoton.Image = ((System.Drawing.Image)(resources.GetObject("pbVideoton.Image")));
+			this.pbVideoton.Location = new System.Drawing.Point(37, 855);
+			this.pbVideoton.Name = "pbVideoton";
+			this.pbVideoton.Size = new System.Drawing.Size(728, 187);
+			this.pbVideoton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pbVideoton.TabIndex = 12;
+			this.pbVideoton.TabStop = false;
+			// 
+			// pbInstagrid
+			// 
+			this.pbInstagrid.Image = ((System.Drawing.Image)(resources.GetObject("pbInstagrid.Image")));
+			this.pbInstagrid.Location = new System.Drawing.Point(37, 1048);
+			this.pbInstagrid.Name = "pbInstagrid";
+			this.pbInstagrid.Size = new System.Drawing.Size(728, 189);
+			this.pbInstagrid.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pbInstagrid.TabIndex = 13;
+			this.pbInstagrid.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.BackColor = System.Drawing.SystemColors.Window;
+			this.ClientSize = new System.Drawing.Size(2286, 1396);
+			this.Controls.Add(this.pbInstagrid);
+			this.Controls.Add(this.pbVideoton);
+			this.Controls.Add(this.chart);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.gbValidationPanel);
+			this.Controls.Add(this.Result_Label);
+			this.Controls.Add(this.videoSourcePlayer1);
+			this.Controls.Add(this.Capture_pictureBox);
+			this.Controls.Add(this.CameraList_comboBox);
+			this.Controls.Add(this.Snapshot_button);
+			this.Controls.Add(this.Satart_button);
+			this.Margin = new System.Windows.Forms.Padding(6);
+			this.Name = "Form1";
+			this.Text = "Battery Cell Orientation Checker - Machine Learning";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.Capture_pictureBox)).EndInit();
+			this.gbValidationPanel.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbVideoton)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbInstagrid)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Satart_button;
+        private System.Windows.Forms.Button Snapshot_button;
+        private System.Windows.Forms.ComboBox CameraList_comboBox;
+        private System.Windows.Forms.PictureBox Capture_pictureBox;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private System.Windows.Forms.Label Result_Label;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.GroupBox gbValidationPanel;
+		private System.Windows.Forms.Button btnCorrect;
+		private System.Windows.Forms.Button btnPPN;
+		private System.Windows.Forms.Button btnPNP;
+		private System.Windows.Forms.Button btnPNN;
+		private System.Windows.Forms.Button btnPPP;
+		private System.Windows.Forms.Button btnNPP;
+		private System.Windows.Forms.Button btnNPN;
+		private System.Windows.Forms.Button btnNNP;
+		private System.Windows.Forms.Button btnNNN;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+		private System.Windows.Forms.PictureBox pbVideoton;
+		private System.Windows.Forms.PictureBox pbInstagrid;
+	}
+}
+
---
+++ Camera/Form1.cs
@@ -0,0 +1,326 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Drawing;
+using System.Collections;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+using CameraML.Model;
+
+
+
+namespace Camera
+{
+
+	public partial class Form1 : Form
+    {
+
+        private string getOrderName(int n)
+		{
+            switch (n)
+			{
+                case 0: return "NNN";
+                case 1: return "NNP";
+                case 2: return "NPN";
+                case 3: return "NPP";
+                case 4: return "PNN";
+                case 5: return "PNP";
+                case 6: return "PPN";
+                case 7: return "PPP";
+                default: return "Wrong data";
+            }
+		}
+        
+
+		private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoDevice;
+        private VideoCapabilities[] snapshotCapabilities;
+        private ArrayList listCamera = new ArrayList();
+        public string pathFolder = Directory.GetCurrentDirectory()+@"\sample\";   //Application.StartupPath + @"\ImageCapture\";
+        public string celldataPath = @"C:\Users\bence\Desktop\MachineLearning\celldata\";
+        public Bitmap seged;
+        private Stopwatch stopWatch = null;
+        private static bool needSnapshot = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            getListCameraUSB();
+            gbValidationPanel.Enabled = false;
+            chart.Titles.Add("Distribution");
+            pbInstagrid.Visible = true;
+            pbVideoton.Visible = true;
+        }
+
+        private static string _usbcamera;
+        public string usbcamera
+        {
+            get { return _usbcamera; }
+            set { _usbcamera = value; }
+        }
+
+        private void Satart_button_Click(object sender, EventArgs e)
+        {
+            OpenCamera();
+        }
+
+        
+
+        private void OpenCamera()
+        {
+            try
+            {
+                usbcamera = CameraList_comboBox.SelectedIndex.ToString();
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+                if (videoDevices.Count != 0)
+                {
+                    // add all devices to combo
+                    foreach (FilterInfo device in videoDevices)
+                    {
+                        listCamera.Add(device.Name);
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Camera devices found");
+                }
+
+                videoDevice = new VideoCaptureDevice(videoDevices[Convert.ToInt32(usbcamera)].MonikerString);
+                snapshotCapabilities = videoDevice.SnapshotCapabilities;
+                if (snapshotCapabilities.Length == 0)
+                {
+                    //MessageBox.Show("Camera Capture Not supported");
+                }
+
+                OpenVideoSource(videoDevice);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+        }
+
+        //Delegate Untuk Capture, insert database, update ke grid 
+        public delegate void CaptureSnapshotManifast(Bitmap image);
+        public void UpdateCaptureSnapshotManifast(Bitmap image)
+        {
+            try
+            {
+                needSnapshot = false;
+                Capture_pictureBox.Image = image;
+                Capture_pictureBox.Update();
+
+
+                string nameImage = "sampleImage";
+                string nameCapture = nameImage + ".jpg";    // "_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".jpg";
+
+                if (Directory.Exists(pathFolder))
+                {
+                    Capture_pictureBox.Image.Save(pathFolder + nameCapture, ImageFormat.Jpeg);
+                }
+                else
+                {
+                    Directory.CreateDirectory(pathFolder);
+                    Capture_pictureBox.Image.Save(pathFolder + nameCapture, ImageFormat.Jpeg);
+                }
+
+
+                var input = new ModelInput()
+                {
+                    ImageSource = Directory.GetCurrentDirectory() + @"\sample\sampleImage.jpg"
+                };
+                ModelOutput result = ConsumeModel.Predict(input);
+                Console.WriteLine("Tipp: " + result.Prediction);
+                Result_Label.Text = "Prediction: " + result.Prediction;
+                chart.Series["s1"].Points.Clear();
+                for (int i = 0; i < 4; i++){
+                    chart.Series["s1"].Points.AddXY(getOrderName(i) +" "+ String.Format("{0:00.00}", (result.Score[i] * 100)) + "%", (result.Score[i] * 100));
+                    chart.Series["s1"].Points.AddXY(getOrderName(i + 4) + " " + String.Format("{0:00.00}", (result.Score[i+4] * 100)) + "%", (result.Score[i+4] * 100));
+                }
+                chart.Series["s1"].Sort(System.Windows.Forms.DataVisualization.Charting.PointSortOrder.Descending);
+            }
+
+            catch(Exception ex) { Console.WriteLine(ex.Message); }
+
+        }
+
+        public void OpenVideoSource(IVideoSource source)
+        {
+            try
+            {
+                // set busy cursor
+                this.Cursor = Cursors.WaitCursor;
+
+                // stop current video source
+                CloseCurrentVideoSource();
+
+                // start new video source
+                videoSourcePlayer1.VideoSource = source;
+                videoSourcePlayer1.Start();
+
+                // reset stop watch
+                stopWatch = null;
+
+
+                this.Cursor = Cursors.Default;
+            }
+            catch { }
+        }
+
+        private void getListCameraUSB()
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            if (videoDevices.Count != 0)
+            {
+                // add all devices to combo
+                foreach (FilterInfo device in videoDevices)
+                {
+                    CameraList_comboBox.Items.Add(device.Name);
+                }
+            }else
+            {
+                CameraList_comboBox.Items.Add("No DirectShow devices found");
+            }
+
+            CameraList_comboBox.SelectedIndex = 0;
+
+        }
+
+        public void CloseCurrentVideoSource()
+        {
+            try
+            {
+
+                if (videoSourcePlayer1.VideoSource != null)
+                {
+                    videoSourcePlayer1.SignalToStop();
+
+                    // wait ~ 3 seconds
+                    for (int i = 0; i < 30; i++)
+                    {
+                        if (!videoSourcePlayer1.IsRunning)
+                            break;
+                        System.Threading.Thread.Sleep(100);
+                    }
+
+                    if (videoSourcePlayer1.IsRunning)
+                    {
+                        videoSourcePlayer1.Stop();
+                    }
+
+                    videoSourcePlayer1.VideoSource = null;
+                }
+            }
+            catch { }
+        }
+
+        private void Snapshot_button_Click(object sender, EventArgs e)
+        {
+            needSnapshot = true;
+            Snapshot_button.Enabled = false;
+            gbValidationPanel.Enabled = true;
+        }
+
+        private void videoSourcePlayer1_NewFrame(object sender, ref Bitmap image)
+        {
+            try
+            {
+                DateTime now = DateTime.Now;
+                Graphics g = Graphics.FromImage(image);
+
+                // paint current time
+                SolidBrush brush = new SolidBrush(Color.Red);
+                g.DrawString(now.ToString(), this.Font, brush, new PointF(5, 5));
+                brush.Dispose();
+                if (needSnapshot)
+                {
+                    seged = image;
+                    this.Invoke(new CaptureSnapshotManifast(UpdateCaptureSnapshotManifast), image);
+                }
+                g.Dispose();
+            }
+            catch
+            { }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CloseCurrentVideoSource();
+        }
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnNNN_Click(object sender, EventArgs e)
+		{
+            System.IO.File.Move(pathFolder + "sampleImage.jpg", celldataPath + @"nnn\" + "NNN_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".jpg");
+            Snapshot_button.Enabled = true;
+            gbValidationPanel.Enabled = false;
+        }
+
+		private void btnNNP_Click(object sender, EventArgs e)
+		{
+            System.IO.File.Move(pathFolder + "sampleImage.jpg", celldataPath + @"nnp\" + "NNP_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".jpg");
+            Snapshot_button.Enabled = true;
+            gbValidationPanel.Enabled = false;
+        }
+
+		private void btnNPN_Click(object sender, EventArgs e)
+		{
+            System.IO.File.Move(pathFolder + "sampleImage.jpg", celldataPath + @"npn\" + "NPN_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".jpg");
+            Snapshot_button.Enabled = true;
+            gbValidationPanel.Enabled = false;
+        }
+
+		private void btnNPP_Click(object sender, EventArgs e)
+		{
+            System.IO.File.Move(pathFolder + "sampleImage.jpg", celldataPath + @"npp\" + "NPP_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".jpg");
+            Snapshot_button.Enabled = true;
+            gbValidationPanel.Enabled = false;
+        }
+
+		private void btnPPP_Click(object sender, EventArgs e)
+		{
+            System.IO.File.Move(pathFolder + "sampleImage.jpg", celldataPath + @"ppp\" + "PPP_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".jpg");
+            Snapshot_button.Enabled = true;
+            gbValidationPanel.Enabled = false;
+        }
+
+		private void btnPNN_Click(object sender, EventArgs e)
+		{
+            System.IO.File.Move(pathFolder + "sampleImage.jpg", celldataPath + @"pnn\" + "PNN_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".jpg");
+            Snapshot_button.Enabled = true;
+            gbValidationPanel.Enabled = false;
+        }
+
+		private void btnPNP_Click(object sender, EventArgs e)
+		{
+            System.IO.File.Move(pathFolder + "sampleImage.jpg", celldataPath + @"pnp\" + "PNP_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".jpg");
+            Snapshot_button.Enabled = true;
+            gbValidationPanel.Enabled = false;
+        }
+
+		private void btnPPN_Click(object sender, EventArgs e)
+		{
+            System.IO.File.Move(pathFolder + "sampleImage.jpg", celldataPath + @"ppn\" + "PPN_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".jpg");
+            Snapshot_button.Enabled = true;
+            gbValidationPanel.Enabled = false;
+        }
+
+		private void btnCorrect_Click(object sender, EventArgs e)
+		{
+            Snapshot_button.Enabled = true;
+            gbValidationPanel.Enabled = false;
+        }
+
+		
+	}
+}
